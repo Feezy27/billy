55a7883add169c4ad671e7cdbd360156ab527fa5
Feezy27 <philippe2714@gmail.com>
2026-09-09 12:41:02 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -21,7 +21,7 @@
 
 // Change ce numero a chaque livraison : il force le nettoyage des
 // anciennes reserves.
-const VERSION = 'billy-2026-09-06e';
+const VERSION = 'billy-2026-09-09b';
 
 const FICHIERS = [
   './',
